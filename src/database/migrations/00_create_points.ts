ff934b5cc327e8dfab31a1ead94cbfe458094b42
Leandro Rocha <prog.leandro@gmail.com>
2020-07-11 23:34:27 -0300

    Cadastro dos pontos de coleta

--- src/database/migrations/00_create_points.ts
+++ src/database/migrations/00_create_points.ts
@@ -5,9 +5,10 @@ export async function up(knex: Knex) {
         table.increments('id').primary();
         table.string('image').notNullable();
         table.string('name').notNullable();
-        table.string('email').notNullable();
-        table.decimal('logitude').notNullable();
+        table.string('email').notNullable()
+        table.string('whatsapp').notNullable();
         table.decimal('latitude').notNullable();
+        table.decimal('longitude').notNullable();
         table.string('city').notNullable();
         table.string('uf', 2).notNullable();        
 
--- src/routes.ts
+++ src/routes.ts
@@ -2,17 +2,58 @@ import express from 'express';
 import knex from './database/connection';
 
 const routes = express.Router()
+
 routes.get('/items', async (req, res) => {    
     const items = await knex('items').select('*');
 
     const serializedItems = items.map(item => {
         return {
+            id: item.id,
             title: item.title,
             image_url: `http://localhost:3333/uploads/${item.image}` 
         }
     })
 
     return res.json(serializedItems);
+});
+
+routes.post('/points', async (req, res) => { 
+    const {
+        name,
+        email,
+        whatsapp,
+        latitude,
+        longitude,
+        city,
+        uf,
+        items
+     } = req.body;
+
+     const trx = await knex.transaction()
+
+     const interts = await trx('points').insert({
+        image: 'image-fake',
+        name,
+        email,
+        whatsapp,
+        latitude,
+        longitude,
+        city,
+        uf,
+     });
+
+     const point_id = interts[0];
+
+     const pointItems =  items.map((item_id: number) => {
+         return {
+             item_id,
+             point_id,
+         }
+     })
+
+     await trx('point_items').insert(pointItems);
+
+    return res.json({ success: true});
  });
 
  export default routes;
